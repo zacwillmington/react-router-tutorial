d6a40ac664566b1a05f7baff4d29564894874059
Zac Willmington <zac.willmington@gmail.com>
2019-02-03 11:02:07 -0800

    Set up components About, Contact and Home.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.zacwillmington.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
+        <HomePage />
+        <AboutPage />
+        <ContactPage />
       </div>
     );
   }
