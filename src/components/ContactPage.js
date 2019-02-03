178a3ef150049009a9ac6a4087885a744df30f22
“ZacWillmington“ <zac.willmington@gmail.com>
2019-02-03 15:50:31 -0800

    NavLinks and Router components ready to be hooked up via Route components.

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const ContactPage = props => {
     return (
-        <div>
+        <div className='card ContactPage'>
             <h1>Contact Page</h1>
+            <p> Small batch intelligentsia master cleanse, green juice hammock chambray schlitz ethical tousled ennui chia mlkshk literally whatever. Venmo slow-carb affogato pinterest chicharrones, live-edge cred vape succulents chia aesthetic .</p>
         </div>
     );
 };
 
-export default ContactPage;
+export default ContactPage; 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div className='NavBar'>
-            
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-tabs">
+                    <li class="nav-item active">
+                    <NavLink className='nav-link' to="/">Home</NavLink>
+                    </li>
+                    <li class="nav-item active">
+                    <NavLink className='nav-link' to="/About">About</NavLink>
+                    </li>
+                    <li class="nav-item active">
+                    <NavLink className='nav-link' to="/Contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
