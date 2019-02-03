ac292b05cf9646e526ffde2de3ab75bfb957b5a7
Zac Willmington <zac.willmington@gmail.com>
2019-02-03 15:50:31 -0800

    NavLinks and Router components ready to be hooked up via Route components.

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AboutPage = props => {
     return (
-        <div>
+        <div className='card AboutPage'>
             <h1>About Page</h1>
+            <p> Small batch intelligentsia master cleanse, green juice hammock chambray schlitz ethical tousled ennui chia mlkshk literally whatever. Venmo slow-carb affogato pinterest chicharrones, live-edge cred vape succulents chia aesthetic keffiyeh. Street art roof party 90's celiac. Activated charcoal bicycle  rights banjo craft beer chambray.</p>
+            <p> Small batch intelligentsia master cleanse, green juice hammock chambray schlitz ethical tousled ennui chia mlkshk literally whatever. Venmo slow-carb affogato pinterest chicharrones, live-edge cred vape succulents chia aesthetic keffiyeh. Street art roof party 90's celiac. Activated charcoal bicycle rights banjo craft beer chambray.</p>
         </div>
     );
 };
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const HomePage = props => {
     return (
-        <div>
+        <div className='card HomePage'>
             <h1>Home Page Content</h1>
+            <p>
+            Lorem ipsum dolor amet iPhone bicycle rights kogi vexillologist VHS, lomo yuccie actually chambray blog hella you probably haven't heard of them YOLO. Meh iPhone man bun, quinoa vice vinyl tbh hot chicken polaroid. Sriracha meggings waistcoat synth asymmetrical vegan fam williamsburg kitsch squid hot chicken small batch church-key. Austin biodiesel meggings echo park umami quinoa coloring book.
+
+            Biodiesel tote bag meggings etsy four dollar toast quinoa la croix artisan. Brunch mumblecore kinfolk, butcher seitan tattooed man bun live-edge snackwave chambray. Poutine quinoa cornhole jianbing helvetica air plant YOLO. Art party snackwave selvage activated charcoal narwhal photo booth master cleanse. Wolf seitan aesthetic edison bulb williamsburg ethical pickled ramps franzen helvetica deep v la croix. Next level tbh twee pork belly raclette put a bird on it snackwave flexitarian VHS aesthetic dreamcatcher mustache meh try-hard.
+
+            Small batch intelligentsia master cleanse, green juice hammock chambray schlitz ethical tousled ennui chia mlkshk literally whatever. Venmo slow-carb affogato pinterest chicharrones, live-edge cred vape succulents chia aesthetic keffiyeh. Street art roof party 90's celiac. Activated charcoal bicycle rights banjo craft beer chambray.
+            </p>
         </div>
     );
 };
